ee70c5dbe26e840c9085d76eb46d2109b800fe56
Kévin Moreau <moreau.kevin1234@gmail.com>
2024-06-19 19:26:37 +0200

    Ajout direction projectile

--- mobs/totem_claquette/BottomTotemClaquette/BottomTotemClaquette.gd
+++ mobs/totem_claquette/BottomTotemClaquette/BottomTotemClaquette.gd
@@ -14,7 +14,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var fusion = false
 var max_totem=2
 var nb_totem=0
-
+var projectile_direction=null
 func _ready():
 	self_width = get_node("HitboxBottomTotem").shape.extents.y * 2
 	self_lenght= get_node("HitboxBottomTotem").shape.extents.x * 2
@@ -22,7 +22,6 @@ func _process(delta):
 	velocity.y += gravity * delta  # Apply the gravity
 	if fusion==true:
 		var direction = (totem.global_position - self.global_position).normalized()
-		velocity.x=direction.x
 		fusion=totem.get("fusion")
 	else:
 		chase_player()
@@ -32,11 +31,13 @@ func _on_detection_area_body_entered(body):
 	if body.is_in_group("Player"):
 		player = body
 		player_chase = true
+		$ProjectileTimer.start()
 
 func _on_detection_area_body_exited(body):
 	if body.is_in_group("Player"):
 		player = null
 		player_chase = false
+		$ProjectileTimer.stop()
 
 func _on_detection_area_totem_body_entered(body):
 	if body.is_in_group("TopTotemClaquette") and body != self:
@@ -51,12 +52,13 @@ func _on_detection_area_totem_body_exited(body):
 #Function to chase the player, the totem goes towards the player and fires a projectile at regular intervals
 func chase_player():
 	if player_chase:
-			$ProjectileTimer.start()
 			var direction = (player.global_position - self.position).normalized()* speed
 			if direction.x>0:
 					get_node("AnimatedSprite2D").flip_h=true
+					projectile_direction=true
 			else:
 					get_node("AnimatedSprite2D").flip_h = false
+					projectile_direction=false
 			velocity.x = direction.x 
 	else:
 		velocity = lerp(velocity, Vector2.ZERO, 0.01)
@@ -66,7 +68,9 @@ func chase_player():
 func _on_projectile_timer_timeout():
 	var b = projectile.instantiate()
 	get_tree().root.add_child(b)
-	b.start(position)
+	b.start(position,projectile_direction)
+	print("Bottom : ")
+	print(projectile_direction)
 	$ProjectileTimer.start()
 
 func death():
